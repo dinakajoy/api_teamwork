e0b51db0acdbb8310f0e3b306ea8b3277c00463e
odinaka-joy <odinakajoy909@gmail.com>
2019-11-13 02:11:06 +0100

    feature: Create gif [Finishes #12]

--- api/v1/test/gif.spec.js
+++ api/v1/test/gif.spec.js
@@ -5,19 +5,19 @@ const fs = require('fs');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-// const app = require('../../app.js');
+const app = require('../../app.js');
 
 describe('On Teamwork API', () => {
   describe('a POST request to "/gifs"', () => {
     it('should check if user is authenticated before adding gif', (done) => {
-      chai.request('http://127.0.0.1:3000/api/v1/gifs')
-        .post('/')
+      chai.request(app)
+        .post('/api/v1/gifs')
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .set({ Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU3MzU2ODA5NSwiZXhwIjoxNTczNjU0NDk1fQ.0iGYd7Rh7wPiG24Kwtq_clG_82iIvOPlYIVgZJUZNKc' })
         .field('title', 'my title')
         .attach('gif', fs.readFileSync('./api/v1/test/images/gif1.gif'), 'gif1.gif')
         .then((res) => {
-          expect(res.status).to.equal(201);
+          expect(res.status).to.equal(300);
           expect(res.body.data).to.include({
             message: 'GIF image successfully posted'
           });
