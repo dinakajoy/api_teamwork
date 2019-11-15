22d45bd92f6df3ea34786e670c59b148ea68aad9
odinaka-joy <odinakajoy909@gmail.com>
2019-11-15 23:37:42 +0100

    feature: add comment to gif and article [Finishes #24]

--- api/v1/routes/articleRoutes.js
+++ api/v1/routes/articleRoutes.js
@@ -8,5 +8,6 @@ const articleController = require('../controllers/ArticleController');
 router.post('/', auth, articleController.createArticle);
 router.patch('/:articleId', auth, articleController.editArticle);
 router.delete('/:articleId', auth, articleController.deleteArticle);
+router.post('/:articleId/comment', auth, articleController.commentArticle);
 
 module.exports = router;
--- api/v1/test/article.spec.js
+++ api/v1/test/article.spec.js
@@ -73,4 +73,23 @@ describe('On Teamwork API', () => {
       done();
     });
   });
+
+  describe('a POST request to "/articles/:articleId/comment"', () => {
+    it('should check if user is authenticated before posting comment for article', (done) => {
+      chai.request(app)
+        .delete('/api/v1/articles/1/comment')
+        .set('Content-Type', 'application/json')
+        .set({ Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU3MzU2ODA5NSwiZXhwIjoxNTczNjU0NDk1fQ.0iGYd7Rh7wPiG24Kwtq_clG_82iIvOPlYIVgZJUZNKc' })
+        .then((res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.data).to.include({
+            message: 'Comment successfully created'
+          });
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+      done();
+    });
+  });
 });
